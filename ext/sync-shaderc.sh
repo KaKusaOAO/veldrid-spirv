09a6f40d91bc21bc009ff19422f11264494aeffc
Kaka <ka.kusaowo@gmail.com>
2024-03-21 00:58:14 +0800

    Update sync-shaderc.sh

--- ext/sync-shaderc.sh
+++ ext/sync-shaderc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 scriptPath="`dirname \"$0\"`"
-
+pip install setuptools
 python $scriptPath/update_shaderc_sources.py --dir $scriptPath/shaderc --file $scriptPath/known_good.json
